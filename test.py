b8b8e5f02adb5507607814e3e5b5efbf0bf8ef4f
Krishnayerne01 <krishnanareshyerne@gmail.com>
2025-03-05 09:03:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-print("Hi My Name is Krishna Yerne")
-print("I am currently pursuing B.E. in Computer Science")
-print("I am in Zeal College of Engineering and Research, Pune")
+print("Hi My Name is Krishna Yerne.")
+print("I am currently pursuing B.E. in Computer Science.")
+print("I am in Zeal College of Engineering and Research, Pune.")
